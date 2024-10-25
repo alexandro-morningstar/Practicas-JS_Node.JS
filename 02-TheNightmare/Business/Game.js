4ad29efeec74272b3b9029665868c309c728b6a3
alexandros-morningstar <alejandro.gh98@outlook.com>
2024-10-25 14:05:33 -0600

    aun no se resuelve el bug del texto >:C

--- 02-TheNightmare/Business/Game.js
+++ 02-TheNightmare/Business/Game.js
@@ -21,28 +21,22 @@ let b_Option;
 
 /* --------------- Llamada de funciones ---------------  */
 function intro() {
+    narrationText = document.getElementById("narration");
     narrationText.innerText = narration.intro();
-    narrationText = document.getElementById("narration")
+    narrationText.addEventListener("click", function() {
+        narrationText.innerText="";
+    });
     // narrationText.addEventListener("click", () => {
     //     narrationText.innerText = "";
     // });
     // setTimeout(() => {
     //     narrationText.innerText=``;
     // }, 5000);
-    //return false;
+    // return false;
 };
 
 function init() {
-    /* ------------ Captura de elementos HTML ------------  */
-    narrationText = document.getElementById("narration");
-    questionText = document.getElementById("question");
-    a_Option = document.getElementById("a-option");
-    choice = document.getElementById("choice");
-    b_Option = document.getElementById("b-option");
-
     /* ------------ Asginación de eventos ------------  */
-    // narrationText.addEventListener("click")
-
     // let testInnerMonster = document.getElementById("ascii-art");
     // testInnerMonster.style.zIndex=0;
     // testInnerMonster.innerText = monsterDrawer.draw_scaryFace();
@@ -51,7 +45,16 @@ function init() {
 
 /* --------------- Evento DOM ---------------  */
 document.addEventListener("DOMContentLoaded", () => { //Inmediatamente despues de la carga del DOM...
-    if (checkPlayerExist() == false) { //...revisamos que se hayan enviado los datos del jugador.
+
+    /* ------------ Captura de elementos HTML justo después de la carga del DOM (independientemente de si se encuentran datos del jugador o no) ------------  */
+    // narrationText = document.getElementById("narration");
+    questionText = document.getElementById("question");
+    a_Option = document.getElementById("a-option");
+    choice = document.getElementById("choice");
+    b_Option = document.getElementById("b-option");
+
+    //...revisamos que se hayan enviado los datos del jugador.
+    if (checkPlayerExist() == false) {
         window.alert("No se han enviado datos del jugador!!");
         window.location="menu.html";
     } else { //Si todo es correcto...
